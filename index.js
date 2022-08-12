d19f338cf917c8d3bd33ea9474e53035fa045f63
Edwar Frank Pons Bessio <edwar.servidor@gmail.com>
2022-08-12 01:42:25 -0300

    Mi primer Ejercicio con Node de Open Bootcamp

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Hola, este es el primer ejercicio con Node en el mejor Bootcamp de programación del mundo.");
+
+curl -X POST 'https://api.github.com/user/repos' --header 'Accept: */*' --header 'Authorization: Bearer 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' --header 'Content-Type: application/json' --data-raw '{"name": "Ejercicio1", "private": false, "description" : ""}'
