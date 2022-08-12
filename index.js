133a73fee2b882feb1a25c52bcd1aa186fb08ab7
Edwar Frank Pons Bessio <edwar.servidor@gmail.com>
2022-08-12 02:06:10 -0300

    Quitamos el CURL

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
 console.log("Hola, este es el primer ejercicio con Node en el mejor Bootcamp de programación del mundo.");
-
-curl -X POST 'https://api.github.com/user/repos' --header 'Accept: */*' --header 'Authorization: Bearer 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' --header 'Content-Type: application/json' --data-raw '{"name": "Ejercicio1", "private": false, "description" : ""}'
